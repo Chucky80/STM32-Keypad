b801a36b62851f7143095644ac41b054ae2e0878
Chucky80 <68803540+Chucky80@users.noreply.github.com>
2023-02-26 10:05:55 +0100

    Create Keypad.h

---
+++ Keypad.h
@@ -0,0 +1,83 @@
+#ifndef KEYPAD_H
+#define KEYPAD_H
+
+#include <cstdint>
+#include "stm32f1xx_hal.h"
+
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+
+const char NO_KEY = '\0';
+constexpr auto LIST_MAX = 10;		// Max number of keys on the active list.;
+constexpr auto MAPSIZE = 10;// MAPSIZE is the number of rows (times 16 columns);
+
+typedef enum
+{
+	IDLE, PRESSED, HOLD, RELEASED
+} KeyState;
+
+class Key
+{
+public:
+	// members
+	char kchar;
+	int kcode;
+	KeyState kstate;
+	bool stateChanged;
+
+	// methods
+	Key();
+	Key(char userKeyChar);
+	void key_update(char userKeyChar, KeyState userState, bool userStatus);
+};
+
+typedef struct
+{
+	unsigned char rows;
+	unsigned char columns;
+} KeypadSize;
+
+class Keypad: public Key
+{
+public:
+	Keypad(GPIO_TypeDef *port, char *userKeymap, uint8_t *rowPins, uint8_t *colPins, unsigned char numRows, unsigned char numCols);
+
+	uint16_t bitMap[MAPSIZE];	// 10 row x 16 column array of bits. Except Due which has 32 columns.
+	Key key[LIST_MAX];
+	unsigned long holdTimer;
+
+	char getKey();
+	bool getKeys();
+	KeyState getState();
+	void begin(char *userKeymap);
+	bool isPressed(char keyChar);
+	void setDebounceTime(uint16_t);
+	void setHoldTime(uint16_t);
+	void addEventListener(void (*listener)(char));
+	int findInList(char keyChar);
+	int findInList(int keyCode);
+	char waitForKey();
+	bool keyStateChanged();
+	unsigned char numKeys();
+
+private:
+	uint32_t startTime;
+	char *keymap;
+	uint8_t *_rowPins;
+	uint8_t *_columnPins;
+	KeypadSize sizeKpd;
+	uint16_t debounceTime;
+	uint16_t holdTime;
+	bool single_key;
+	GPIO_TypeDef *_port;
+
+	void scanKeys();
+	bool updateList();
+	void nextKeyState(unsigned char n, bool button);
+	void transitionTo(unsigned char n, KeyState nextState);
+	void (*keypadEventListener)(char);
+};
+
+#endif
