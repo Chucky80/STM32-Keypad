785eecdd9d0acba562fc4c08ed7ca11be977905c
Chucky80 <68803540+Chucky80@users.noreply.github.com>
2023-02-28 01:33:21 +0100

    Update Keypad_4x5.cpp
    Updated to work with new API

--- Examples/Keypad_4x5.cpp
+++ Examples/Keypad_4x5.cpp
@@ -2,11 +2,13 @@
 #include "gpio.h"
 #include "Keypad.h"
 
-uint8_t rowPins[] = {8, 7, 6, 5, 4};  // GPIO Input Pull_Up
-uint8_t colPins[] = {0, 1, 2, 3};     // GPIO Output
 char customKey;
+const unsigned char numRows = 5;
+const unsigned char numCols = 4;
+const char* rowPins[] = { "B0" , "A7", "A6", "A5", "A4" };   // GPIO Input - Pull_Up
+const char* colPins[] = { "A0", "A1", "A2", "A3" };          // GPIO Output
 
-char keypadArray[sizeof(rowPins)][sizeof(colPins)] =
+char keypadArray[numRows][numCols] =
 {
   {'A','B','#','*'},
   {'1','2','3','U'},
@@ -15,13 +17,15 @@ char keypadArray[sizeof(rowPins)][sizeof(colPins)] =
   {'<','0','>','E'}
 };
 
+void SystemClock_Config(void);
+
 int main(void)
 {
   HAL_Init();
   SystemClock_Config();
   MX_GPIO_Init();
 
-  Keypad keypad(GPIOA, (char*)keypadArray, rowPins, colPins, sizeof(rowPins), sizeof(colPins));
+  Keypad keypad((char*)keypadArray, rowPins, colPins, numRows, numCols);
 
   while (1)
   {
@@ -29,7 +33,55 @@ int main(void)
 
   	if(customKey)
   	{
-		// Your code here 
+		// Do something
   	}
   }
 }
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void Error_Handler(void)
+{
+  __disable_irq();
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+void assert_failed(uint8_t *file, uint32_t line)
+{
+}
+#endif /* USE_FULL_ASSERT */
